9f6db4cf272b08ae88cf5166cd8c24a0ad9fbc9d
ananya668 <ananyanidumolu873@gmail.com>
2025-06-02 23:01:46 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+const form =document.querySelector('form');
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+const name =document.querySelector('#name').value;
+const message =document.querySelector('#message').value;
+const messageElement =document.createElement('p');
+messageElement.textContent = 'Thank you for your message, '${name}!' we will be in touch soon.';
+messageElement.classList.add('message');
+form.appendChild(messageElement);
+});
